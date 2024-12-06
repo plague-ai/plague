6857c4c75976f1cb4c82e012e53151b3b5de18f7
Godwin Idemudia <godwinide@gmail.com>
2024-12-06 19:43:05 +0100

    new

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -155,7 +155,7 @@ const WorldMap = () => {
       }
       {loading && <div className='fixed flex justify-center items-center top-0 left-0 w-screen h-screen bg-black'><h1 className='text-white text-3xl font-bold'>Loading, Please Wait...</h1></div>}
       <div className='fixed top-1/4 bg-black bg-opacity-50 right-2 p-4 rounded flex flex-col gap-10'>
-        <Link href="https://x.com/plague_ai"><Image src='/X Logo-01.png' alt='twitter' width={40} height={40}/></Link>
+        <Link href="https://x.com/ai_plague"><Image src='/X Logo-01.png' alt='twitter' width={40} height={40}/></Link>
         <Link href="http://github.com/plague-ai"><Image src='/GitHub Icon-01.png' alt='github' width={40} height={40}/></Link>
       </div>
     </div>
